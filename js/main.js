9c7b2c00415660d5f2604e9a5c819b2b528beae1
Oskar Popik <poos23rz@student.ju.se>
2024-10-25 12:51:31 +0200

    Added collapsing menu for mobile

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-const submitButton = document.getElementById("cont_button");
-const closeButton = document.getElementById("cont_button_close");
-
-submitButton.addEventListener("click", function (openEvent) {
-  /* The following 1 line of code was adapted from https://stackoverflow.com/questions/50193227/basic-react-form-submit-refreshes-entire-page?answertab=scoredesc#tab-top */
-  openEvent.preventDefault(); // prevents default behavior of the form (refresh after clicking submit)
-  document.getElementById("popup_container").style.display = "block";
-  document.getElementById("contact_subpage").style.filter = "blur(3px)";
-});
-
-closeButton.addEventListener("click", function (closeEvent) {
-  /* The following 1 line of code was adapted from https://www.freecodecamp.org/news/javascript-refresh-page-how-to-reload-a-page-in-js/ Accessed: 2023-11-23 */
-  location.reload(true); // reloads the contact.html subpage with bypass of the cache
+// Toggle the navigation menu on small screens
+document.getElementById("menu_toggle").addEventListener("click", function () {
+  document.getElementById("nav_menu").classList.toggle("active");
 });
