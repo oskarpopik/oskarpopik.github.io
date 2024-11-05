bafcfadd4e7c6b8f19e0474d2758f4f38d97e3d3
Oskar Popik <poos23rz@student.ju.se>
2024-11-05 01:47:08 +0100

    Modified the index page with additional content

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@
 document.getElementById("menu_toggle").addEventListener("click", function () {
   document.getElementById("nav_menu").classList.toggle("active");
 });
+
+// Toggle the additional information on the main after the animation is over
+document
+  .querySelector(".animation_container")
+  .addEventListener("animationend", () => {
+    document
+      .querySelector(".information_element_container")
+      .classList.add("information_element_visible");
+    document
+      .querySelector(".footer_menu_index")
+      .classList.add("footer_menu_visible");
+  });
